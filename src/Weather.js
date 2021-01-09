579199033488854f15f16a0ec0fab222c7a1b782
Meagan Louise <meaganledwards@gmail.com>
2021-01-09 22:00:12 +0000

    Added "Feels like" description on app

--- src/Weather.js
+++ src/Weather.js
@@ -19,6 +19,7 @@ export default function Weather() {
         "https://lh3.googleusercontent.com/M4ku99RiIhvhP9nHnPLCwd_5fy6wqHNcP2afBSQce9hxClBVJKaGVMTpJgeb1P4dXhMhPSqXod1KgvhfP8WJr4hHmLuiztT4Uq4gPIwQNeW508FgGSovAnx2CecUqxnQvfDdX6uo177GPVJmdz0kFmBmKjI2TygRIP-G9A3rMWV3pKMfbXlOIWN0B0WW4n9GkXCA3CAYG_n4UBhrzmRUqjg_SD8mdGEOs6COaZGOvZyW9GshVJyRj69sW_G7sMm_QRV1GZcoKDJu3gvgOvcRG4v9Tqu5-RSyUffezPXzsQcrW-To5ziYYrqxRUUNrGuMbEwHyKiUjYGJmgsRckKXXKBPZnnK850ljF47UPRblJ0k75bu8Fzp8GPiKQMU0JhvbdGqM8_8inRhkCJ3CeRFwjNWt4twE2v6mxQgcaj0Ns2ThKRv-apQX79dtLpNyMuet9F6wayz4zKYtJyFTOBXR33EMAffwHTB989WNMmmrHWJeNs8VlPY19CbsdAoCtSTzorp3ECYEnIUSerKdGSvs6-lrOqZDwN8eif5YF9cqWKafCAWi7Gp4CMX2t8RSBX7FCmJFgBAcSoXcWxL695Qx9rAI-zAa7zHjICkm_WOpplglfPTKmZGmGhz_WvEd_jm0q05PDzgFLoTwiWpHxbSmsQTT5NMTceOYIKuASy2t_7-kmgi-fsEVLFgIjX3=w373-h317-no?authuser=0",
       wind: response.data.wind.speed,
       city: response.data.name,
+      feelsLike: response.data.main.feels_like,
     });
   }
 
@@ -44,7 +45,7 @@ export default function Weather() {
               <ul className="date">
                 Tuesday <div className="time">11:11</div>
               </ul>
-              <ul className="WeatherDescription">{weatherData.description}</ul>
+              <ul className="text-capitalize">{weatherData.description}</ul>
             </li>
           </div>
         </div>
@@ -57,14 +58,20 @@ export default function Weather() {
         </div>
 
         <h2>
-          <span className="degree">{weatherData.temperature}°</span>
+          <span className="degree">{Math.round(weatherData.temperature)}°</span>
           <span className="celcius">c|f</span>
         </h2>
         <div className="ConditionsList">
           <li className="conditions">
-            <ul>Precipitation</ul>
-            <ul>Humidity:{weatherData.humidity}</ul>
-            <ul>Wind:{weatherData.wind} mph </ul>
+            <ul>
+              Feels like: {""} {Math.round(weatherData.feelsLike)}°c
+            </ul>
+            <ul>
+              Humidity:{""} {weatherData.humidity} %{" "}
+            </ul>
+            <ul>
+              Wind:{""} {Math.round(weatherData.wind)} mph{" "}
+            </ul>
           </li>
         </div>
       </div>
